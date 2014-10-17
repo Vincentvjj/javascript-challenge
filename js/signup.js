1c9de1e0e356889a41915a95aa679688673f5156
vincent <jonanv@uw.edu>
2014-10-16 17:01:48 -0700

    needs form validation

--- js/signup.js
+++ js/signup.js
@@ -22,5 +22,21 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
+    var occupationSelect = document.getElementById('occupation');
+
+    occupationSelect.addEventListener('change', function() {
+        if(occupationSelect.value == 'other') {
+            document.getElementsByName('occupationOther')[0].style.display = "block";
+        }
+    });
+
+    var noThanksButton = document.getElementById('cancelButton');
+    noThanksButton.addEventListener('click', function() {
+        if(window.confirm('Are u sure?')) {
+            window.location.assign('http://google.com');
+        }
+    });
+
 });
+
 
