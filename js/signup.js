0ec99ddc3c05eabd19642487a410f47d6faad128
vincent <jonanv@uw.edu>
2014-10-20 16:31:18 -0700

    see comments

--- js/signup.js
+++ js/signup.js
@@ -37,6 +37,71 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+
+
+    form.addEventListener('submit', onSubmit);
+
 });
+
+
+
+function onSubmit(evt) {
+    evt.returnValue = validateForm(this);
+    if (evt.returnValue == false && evt.preventDefault) {
+        evt.preventDefault();
+    }
+
+    return evt.returnValue;
+}
+
+function validateForm(form) {
+    var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'zip', 'birthdate'];
+    var formValid = true; 
+    var occupationSelect = document.getElementById('occupation');
+    var occupationOther = document.getElementsByName('occupationOther')[0];
+    var zipRegEx = new RegExp('^\\D+$'); // change the regular expression
+
+    if(occupationSelect.value == 'other' && occupationOther.value.trim() == '') {
+        formValid = false;
+        occupationOther.className = "form-control invalid-field";
+    }
+
+    if(!zipRegEx.test) // test it with zip code's vlock
+
+    for(var i = 0; i < requiredFields.length; i++) {
+        formValid &= validateRequiredField(form.elements[requiredFields[i]]);
+    }
+
+    
+
+
+
+    if(!formValid) {
+       alert();
+    }
+
+    return formValid;
+}
+
+
+function validateRequiredField(field) {
+    var value = field.value.trim();
+    var valid = value.length > 0;
+
+    if(!valid) {
+        field.className = 'form-control invalid-field';
+    }
+
+    else {
+        field.className = 'form-control';
+    }
+
+    return valid;
+
+}
+
+    
+
+
 
 
